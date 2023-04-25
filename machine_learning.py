8d53fdddde2a276bf44af31f273c36d0e726a2d6
HuszarDora <dora.huszar@stud.uni-corvinus.hu>
2023-04-25 10:56:45 +0200

    2.rész

--- machine_learning.py
+++ machine_learning.py
@@ -7,6 +7,14 @@ def generate_synthetic_data(x, coefficients, seed=42, noise_std=1):
     y = np.polyval(coefficients[::-1], x) + np.random.normal(0, noise_std, len(x))
     return x, y
 
+
+def visualize_data(x,y):
+    plt.scatter(x,y)
+    plt.xlabel("Feature (x)")
+    plt.ylabel("Target (y)")
+    plt.title("Synthetic Data with Polynomial Relationship and Noise")
+    plt.show()
+
 def main():
     coefficients=[1,0.02,-0.002,0.014]
     x_values=np.linspace(-10,10,100)
