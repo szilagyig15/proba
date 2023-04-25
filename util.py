753a96b7f450fe375a989f1cfd123a5baa7db3d7
Salamon András <andras.salamon@stud.uni-corvinus.hu>
2023-04-25 10:28:16 +0200

    fjani

--- util.py
+++ util.py
@@ -8,9 +8,11 @@ def kivon(a,b):
 
 
 class Kukac:
-    def __init__(self, length, girth):
+    def __init__(self, length, girth, name):
         self.length = length
         self.girth = girth
+        self.name=name
 
+fj=Kukac(20,5,"Fasz János")
 
 print("Hi")
