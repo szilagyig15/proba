9a0acc3bc229c251e36cb336f483a9c245ca0a7c
Szilagyi Gergo <gergo.szilagyi@stud.uni-corvinus.hu>
2023-04-25 10:58:21 +0200

    vesszok

--- machine_learning.py
+++ machine_learning.py
@@ -8,15 +8,20 @@ def generate_synthetic_data(x, coefficients, seed=42, noise_std=1):
     return x, y
 
 
-def visualize_data(x,y):
-    plt.scatter(x,y)
+def visualize_data(x, y):
+    plt.scatter(x, y)
     plt.xlabel("Feature (x)")
     plt.ylabel("Target (y)")
     plt.title("Synthetic Data with Polynomial Relationship and Noise")
     plt.show()
 
+
 def main():
-    coefficients=[1,0.02,-0.002,0.014]
-    x_values=np.linspace(-10,10,100)
-    x,y=generate_synthetic_data(x_values,coefficients)
-    visualize_data(x,y)
+    coefficients=[1, 0.02, -0.002, 0.014]
+    x_values=np.linspace(-10, 10, 100)
+    x, y = generate_synthetic_data(x_values, coefficients)
+    visualize_data(x, y)
+
+
+if __name__ == '__main__':
+    main()
