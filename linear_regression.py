064941e5e95c894d360fc940f21d24ab569ff480
Szilagyi Gergo <gergo.szilagyi@stud.uni-corvinus.hu>
2023-04-25 11:53:14 +0200

    elso 2 fv

--- linear_regression.py
+++ linear_regression.py
@@ -4,7 +4,6 @@ from sklearn.linear_model import LinearRegression
 from machine_learning import generate_synthetic_data
 
 
-
 def fit_linear_regression(x, y):
     lr = LinearRegression()
     lr.fit(x.reshape(-1, 1), y)
--- polynomial_regression.py
+++ polynomial_regression.py
@@ -5,3 +5,22 @@ from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
 from machine_learning import generate_synthetic_data
 
+
+def fit_polynomial_regression(x, y, degree):
+    polynomial_regression = make_pipeline(PolynomialFeatures(degree), LinearRegression())
+    polynomial_regression.fit(x.reshape(-1, 1), y)
+    return polynomial_regression
+
+
+def visualize_data_and_fit(x, y, model, degrees):
+    plt.scatter(x, y)
+    plt.xlabel("Feature (x)")
+    plt.ylabel("Target (y)")
+    plt.title("Synthetic Data with Polynomial Relationship and Noise")
+    x_pred = np.linspace(min(x), max(x), len(x)).reshape(-1, 1)
+    colors = ['red', 'blue', 'green']
+    for model, degree, color in zip(model, degrees, colors):
+        y_pred = model.predict(x_pred)
+        plt.plot(x_pred, y_pred, color=color, label=f'Polynomial Regression (degree {degree})')
+    plt.legend()
+    plt.show()
